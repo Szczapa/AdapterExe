cf26caff3a01beb5090bb8273a4fa6378f5053ae
Szczapa <98310905+Szczapa@users.noreply.github.com>
2024-12-04 16:00:54 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,21 +6,18 @@ public class Main {
     public static void main(String[] args) {
         CurrencyAdapter currencyAdapter = new CurrencyAdapter();
 
-        // Conversion EUR -> USD
         double euros = 100;
         System.out.println(euros + " EUR = "
                 + currencyAdapter.convert("EUR", "USD", euros) + " USD");
 
-        // Conversion USD -> EUR
         double dollars = 100;
         System.out.println(dollars + " USD = "
                 + currencyAdapter.convert("USD", "EUR", dollars) + " EUR");
 
-        // Conversion USD -> GBP
+
         System.out.println(dollars + " USD = "
                 + currencyAdapter.convert("USD", "GBP", dollars) + " GBP");
 
-        // Conversion GBP -> USD
         double gbp = 100;
         System.out.println(gbp + " GBP = "
                 + currencyAdapter.convert("GBP", "USD", gbp) + " USD");
